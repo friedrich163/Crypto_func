542f12742628cab9933face61ec2e16722eabdb1
Wen-Wei0914 <r08943171@g.ntu.edu.tw>
2021-11-25 20:01:13 +0800

    Init run_ecdsa_sign.tcl

---
+++ ecdsa_sign/run_ecdsa_sign.tcl
@@ -0,0 +1,14 @@
+open_project hls_ecdsa_sign_prj -reset
+add_files  src/ecdsa_tb.cpp
+add_files -tb src/ecdsa_tb.cpp
+set_top ecdsa_sign
+
+open_solution "solution0" -reset
+#set_part {xc7z020clg484-1}
+set_part {xczu9eg-ffvb1156-2-e}
+create_clock -period 5 -name default
+csim_design -ldflags "-lcrypto -lssl"
+csynth_design 
+cosim_design -ldflags "-lcrypto -lssl"
+export_design -format ip_catalog
+close_project
