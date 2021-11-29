1b2390b2074217a3bc74973fd9b4b3f9b6756c1c
Wen-Wei0914 <r08943171@g.ntu.edu.tw>
2021-11-29 12:56:45 +0800

    run_ecc_enc.tcl

---
+++ ecc_enc/run_ecc_enc.tcl
@@ -0,0 +1,14 @@
+open_project hls_ecc_enc_prj -reset
+add_files  src/ecc_enc_tb.cpp
+add_files -tb src/ecc_enc_tb.cpp
+set_top ecc_enc
+
+open_solution "solution0" -reset
+#set_part {xc7z020clg484-1}
+set_part {xczu9eg-ffvb1156-2-e}
+create_clock -period 5 -name default
+csim_design -ldflags "-lcrypto -lssl"
+csynth_design 
+cosim_design -ldflags "-lcrypto -lssl"
+export_design -format ip_catalog
+close_project
