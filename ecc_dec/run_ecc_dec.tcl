6b4a6338f30d4e9eff50868a6908b5fdf2521224
Wen-Wei0914 <r08943171@g.ntu.edu.tw>
2021-11-29 13:02:20 +0800

    new file:   run_ecc_dec.tcl

---
+++ ecc_dec/run_ecc_dec.tcl
@@ -0,0 +1,14 @@
+open_project hls_ecc_dec_prj -reset
+add_files  src/ecc_dec_tb.cpp
+add_files -tb src/ecc_dec_tb.cpp
+set_top ecc_dec
+
+open_solution "solution0" -reset
+#set_part {xc7z020clg484-1}
+set_part {xczu9eg-ffvb1156-2-e}
+create_clock -period 5 -name default
+csim_design -ldflags "-lcrypto -lssl"
+csynth_design 
+cosim_design -ldflags "-lcrypto -lssl"
+export_design -format ip_catalog
+close_project
